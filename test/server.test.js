297b09159eb5ff480cc381c3b88a9d27b3b2f493
David Raynes <rayners@gmail.com>
2019-02-04 20:47:32 -0500

    Fixup tests

--- test/server.test.js
+++ test/server.test.js
@@ -10,14 +10,14 @@ describe('server', function() {
     it('should respond with "Hello world!" on /', function() {
         return request(server)
             .get('/')
-            .expect(200, 'Hello world!');
+            .expect(200, /Hello FSWD!/);
     });
 
     ['David', 'John', 'Lee'].forEach(function(name) {
         it('should respond with "Hello, ' + name + '!" on /' + name, function() {
             return request(server)
                 .get('/' + name)
-                .expect(200, 'Hello, ' + name + '!');
+                .expect(200, new RegExp('Hello, ' + name + '!'));
         });
     });
     //
